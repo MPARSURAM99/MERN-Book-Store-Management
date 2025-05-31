9145494c3e5ce8a52667a966ce0e798c6b2561f5
PARSURAM MAHARANA <mparsuram99@gmail.com>
2025-06-01 01:04:15 +0530

    docker container build success

--- backend/server/config.js
+++ backend/server/config.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
-export const mongodbURI = process.env.DB_URI;
+export const mongodbURI = process.env.MONGODB_URI;
